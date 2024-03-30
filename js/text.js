62efa69af36fa005b849b0e660ad575d10bde7a5
arifulislam991 <arifulislam99101@gmail.com>
2024-03-31 02:26:28 +0600

    first commit

---
+++ js/text.js
@@ -0,0 +1,14 @@
+const car ={
+    name:"BMW",
+    model:500,
+    withte:"883kg",
+    color :"red",
+    start:function(){
+        console.log("car start")
+    },
+    drive: function (){
+        console.log("drive the car")
+    },
+}
+console.log(car.color);
+car.start();
